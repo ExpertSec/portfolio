d2164f7b583275dadca6656c0a3588d76b418c6f
linksys <linksys@core>
2024-12-18 15:02:34 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const words = [
+  "desainer grafis",
+  "IOT Engineer",
+  "Software Developer",
+  "Data Analyst",
+  "Machine Learning Engineer",
+  "Data Scientist",
+  "Cybersecurity Specialist",
+  "Web Developer",
+];
+const span = document.getElementById("randomtext");
+let random = () => {
+  let acak = setInterval(() => {
+    const randomText = Math.floor(Math.random() * words.length);
+    span.textContent = words[randomText];
+  }, 15);
+  setTimeout(() => {
+    clearInterval(acak);
+  }, 1000);
+  setTimeout(() => {
+    random();
+  }, 2000);
+};
+random();
